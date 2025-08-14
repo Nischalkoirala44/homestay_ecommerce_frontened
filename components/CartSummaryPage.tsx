271bfdc7dee827241b3849ae7024b9adf385a1aa
Nischalkoirala44 <davidkoirala66@gmail.com>
2025-08-14 17:26:45 +0545

    Create CartSummaryPage.tsx

---
+++ components/CartSummaryPage.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import CartPage from "./CartItem";
+import { useCart } from "../hooks/useCart";
+
+interface CartSummaryPageProps {
+  onClose: () => void;
+}
+
+const CartSummaryPage = ({ onClose }: CartSummaryPageProps) => {
+  const { cartItems, loading, error, increaseQuantity, decreaseQuantity, clearCart } = useCart();
+
+  if (loading) return <p className="text-center mt-20">Loading your cart...</p>;
+  if (error) return <p className="text-center mt-20 text-red-600">Error: {error}</p>;
+
+  return (
+    <div className="w-full max-w-lg h-screen bg-white shadow-lg fixed right-0 top-0 flex flex-col z-50">
+      <CartPage
+        cartItems={cartItems}
+        onClearCart={clearCart}
+        onClose={onClose}
+
+        onIncrease={increaseQuantity}
+        onDecrease={decreaseQuantity}
+      />
+    </div>
+  );
+};
+
+export default CartSummaryPage;
