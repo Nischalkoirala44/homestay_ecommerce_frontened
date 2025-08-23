a1d6c606a1a7f5db1976e5a2d18ac10035542547
christopher7777777 <pokharelchristopher@gmail.com>
2025-08-23 21:14:43 +0545

    make change in product

--- types/Product.ts
+++ types/Product.ts
@@ -1,11 +1,15 @@
+// types/Product.ts
 export interface Product {
-    
     id: string;
     name: string;
     price: string;
     image: string;
     description: string;
     categoryId: number;
-    
+    category?: {
+        id: number;
+        name: string;
+        image?: string;
+        description?: string;
+    };
 }
-
